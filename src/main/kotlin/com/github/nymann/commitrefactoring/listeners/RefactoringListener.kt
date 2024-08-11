c71b03d97232ee2384a65c8e4797b14f1f028a04
Kristian Jakobsen <kristian@nymann.dev>
2024-08-12 01:17:07 +0200

    Refactoring: ChangeSignature PRIMARY_CONSTRUCTOR

--- src/main/kotlin/com/github/nymann/commitrefactoring/listeners/RefactoringListener.kt
+++ src/main/kotlin/com/github/nymann/commitrefactoring/listeners/RefactoringListener.kt
@@ -1,11 +1,10 @@
 package com.github.nymann.commitrefactoring.listeners
 
 import com.github.nymann.commitrefactoring.RefactoringStore
-import com.intellij.openapi.project.Project
 import com.intellij.refactoring.listeners.RefactoringEventData
 import com.intellij.refactoring.listeners.RefactoringEventListener
 
-class RefactoringListener(private val project: Project) : RefactoringEventListener {
+class RefactoringListener : RefactoringEventListener {
     override fun refactoringDone(refactoringId: String, afterData: RefactoringEventData?) {
         RefactoringStore.addRefactoring(refactoringId, afterData)
     }
