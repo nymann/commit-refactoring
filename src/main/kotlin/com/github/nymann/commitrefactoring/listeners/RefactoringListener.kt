fd6e9ce2362549a68c6b737e14a95cd4522ce9a8
Kristian Jakobsen <kristian@nymann.dev>
2024-08-11 21:28:41 +0200

    Remove unused methods

--- src/main/kotlin/com/github/nymann/commitrefactoring/listeners/RefactoringListener.kt
+++ src/main/kotlin/com/github/nymann/commitrefactoring/listeners/RefactoringListener.kt
@@ -2,26 +2,11 @@ package com.github.nymann.commitrefactoring.listeners
 
 import com.github.nymann.commitrefactoring.RefactoringStore
 import com.intellij.openapi.actionSystem.*
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.refactoring.listeners.RefactoringEventData
 import com.intellij.refactoring.listeners.RefactoringEventListener
 
 class RefactoringListener(private val project: Project) : RefactoringEventListener {
-    private val logger = Logger.getInstance(RefactoringListener::class.java)
-
-    override fun refactoringStarted(refactoringId: String, beforeData: RefactoringEventData?) {
-        logger.warn("Refactoring started: $refactoringId")
-    }
-
-    override fun conflictsDetected(refactoringId: String, conflictsData: RefactoringEventData) {
-        logger.warn("Conflicts detected in refactoring: $refactoringId")
-    }
-
-    override fun undoRefactoring(refactoringId: String) {
-        logger.warn("Undo refactoring: $refactoringId")
-    }
-
     override fun refactoringDone(refactoringId: String, afterData: RefactoringEventData?) {
         RefactoringStore.addRefactoring(refactoringId, afterData)
         if (false) {
