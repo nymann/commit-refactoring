95444e9363063e454e3a5df387dd5fe3c1453ad5
Kristian Jakobsen <kristian@nymann.dev>
2024-08-13 00:20:16 +0200

    Use default commit message

--- src/main/java/com/github/nymann/commitrefactoring/InplaceRenameCommitMessageFactory.java
+++ src/main/java/com/github/nymann/commitrefactoring/InplaceRenameCommitMessageFactory.java
@@ -1,20 +1,21 @@
 package com.github.nymann.commitrefactoring;
 
-import com.github.nymann.commitrefactoring.messages.*;
+import com.github.nymann.commitrefactoring.messages.DefaultCommitMessage;
+import com.github.nymann.commitrefactoring.messages.RenameClassCommitMessage;
+import com.github.nymann.commitrefactoring.messages.RenameMethodCommitMessage;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiMethod;
 
 public class InplaceRenameCommitMessageFactory {
     public static CommitMessage create(Refactoring refactoring) {
         PsiElement before = refactoring.getBefore();
-        if(before instanceof PsiMethod) {
+        if (before instanceof PsiMethod) {
             return new RenameMethodCommitMessage(refactoring);
         }
-        if(before instanceof PsiClass) {
+        if (before instanceof PsiClass) {
             return new RenameClassCommitMessage(refactoring);
         }
-        throw new RuntimeException("InplaceRename not supported for: " + before.getClass());
+        return new DefaultCommitMessage(refactoring);
     }
 }
