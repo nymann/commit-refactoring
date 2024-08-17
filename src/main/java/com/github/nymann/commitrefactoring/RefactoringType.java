42a35f3f83c65f4ca79e4823e103f0bb9eb7f5f5
Kristian Jakobsen <kristian@nymann.dev>
2024-08-17 13:39:44 +0200

    Fix deprecation
    This job uses deprecated functionality from the gradle/actions/setup-gradle action. Follow the links for upgrade details.
    
        The `gradle-home-cache-cleanup` input parameter has been replaced by `cache-cleanup`

---
+++ src/main/java/com/github/nymann/commitrefactoring/RefactoringType.java
@@ -0,0 +1,11 @@
+package com.github.nymann.commitrefactoring;
+
+public enum RefactoringType {
+    CHANGE_SIGNATURE,
+    EXTRACT,
+    INLINE,
+    INTRODUCE_PARAMETER,
+    MOVE,
+    RENAME,
+    SAFE_DELETE,
+}
