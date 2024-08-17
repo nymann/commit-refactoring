4c82d3834c8f1d03e7d2b4d0faf9eea971960d73
Kristian Jakobsen <kristian@nymann.dev>
2024-08-17 20:08:31 +0200

    More reliable way to detect new commits

--- src/main/java/com/github/nymann/commitrefactoring/intellij/extensions/RefactoringCheckinHandlerFactory.java
+++ src/main/java/com/github/nymann/commitrefactoring/intellij/extensions/RefactoringCheckinHandlerFactory.java
@@ -1,5 +1,6 @@
 package com.github.nymann.commitrefactoring.intellij.extensions;
 
+import com.github.nymann.commitrefactoring.intellij.IntelliJRefactoringService;
 import com.intellij.openapi.vcs.CheckinProjectPanel;
 import com.intellij.openapi.vcs.changes.CommitContext;
 import com.intellij.openapi.vcs.checkin.CheckinHandler;
@@ -9,11 +10,20 @@ import org.jetbrains.annotations.NotNull;
 public class RefactoringCheckinHandlerFactory extends CheckinHandlerFactory {
     @Override
     public @NotNull CheckinHandler createHandler(@NotNull CheckinProjectPanel panel, @NotNull CommitContext commitContext) {
-        return new RefactoringCheckinHandler();
+        return new RefactoringCheckinHandler(panel.getProject().getService(IntelliJRefactoringService.class));
     }
+
     private static class RefactoringCheckinHandler extends CheckinHandler {
+
+        private final IntelliJRefactoringService service;
+
+        private RefactoringCheckinHandler(IntelliJRefactoringService service1) {
+            service = service1;
+        }
+
         @Override
         public void checkinSuccessful() {
+            service.clearRefactorings();
             super.checkinSuccessful();
         }
     }
