5167fc37e18a816b7e2d09dff2189490974d138e
Kristian Jakobsen <kristian@nymann.dev>
2024-08-17 21:20:09 +0200

    Add RefactoringTestBuilder

---
+++ src/test/java/com/github/nymann/commitrefactoring/RefactoringTestBuilder.java
@@ -0,0 +1,42 @@
+package com.github.nymann.commitrefactoring;
+
+public class RefactoringTestBuilder {
+    private RefactoringType refactoringType = RefactoringType.None;
+    private String beforeName = null;
+    private String afterName = null;
+    private CodeElementType beforeType = CodeElementType.UNKNOWN;
+    private CodeElementType afterType = CodeElementType.UNKNOWN;
+
+    public RefactoringTestBuilder refactoringType(RefactoringType refactoringType) {
+        this.refactoringType = refactoringType;
+        return this;
+    }
+
+    public RefactoringTestBuilder beforeName(String beforeName) {
+        this.beforeName = beforeName;
+        return this;
+    }
+
+    public RefactoringTestBuilder afterName(String afterName) {
+        this.afterName = afterName;
+        return this;
+    }
+
+    public RefactoringTestBuilder beforeType(CodeElementType beforeType) {
+        this.beforeType = beforeType;
+        return this;
+    }
+
+    public RefactoringTestBuilder afterType(CodeElementType afterType) {
+        this.afterType = afterType;
+        return this;
+    }
+
+    public Refactoring build() {
+        return new Refactoring(
+                refactoringType,
+                new CodeElement(beforeName, beforeType),
+                new CodeElement(afterName, afterType)
+        );
+    }
+}
